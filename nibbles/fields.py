4e11b7f4dd63b2ddb6f3811065c6a31a48bf1419
Patrick Cloke <clokep@patrick.cloke.us>
2015-05-27 12:59:18 -0400

    Add initial subclassing.

--- nibbles/fields.py
+++ nibbles/fields.py
@@ -1,12 +1,20 @@
 from collections import OrderedDict
 
-class Field(object):
+
+class BaseField(object):
+    """The actual implementation of a Field should go here, Field simply exists
+    to properly set __metaclass__."""
 
     # Tracks each time a Field instance is created. Used to retain order.
     creation_counter = 0
 
+    def __init__(self):
+        # Increase the creation counter, and save our local copy.
+        self.creation_counter = Field.creation_counter
+        BaseField.creation_counter += 1
 
-class ModelBase(type):
+
+class _MetaField(type):
     """
     Metaclass that collects Fields declared on the base classes.
 
@@ -16,13 +24,13 @@ class ModelBase(type):
         # Collect fields from current class.
         current_fields = []
         for key, value in list(attrs.items()):
-            if isinstance(value, Field):
+            if isinstance(value, BaseField):
                 current_fields.append((key, value))
                 attrs.pop(key)
         current_fields.sort(key=lambda x: x[1].creation_counter)
         attrs['declared_fields'] = OrderedDict(current_fields)
 
-        new_class = (super(ModelBase, mcs)
+        new_class = (super(_MetaField, mcs)
             .__new__(mcs, name, bases, attrs))
 
         # Walk through the MRO.
@@ -43,27 +51,5 @@ class ModelBase(type):
         return new_class
 
 
-class Model(object):
-    __metaclass__ = ModelBase
-
-
-class Model1(Model):
-    a = Field()
-    b = Field()
-    c = Field()
-    z = Field()
-
-class Model2(Model):
-    c = Field()
-    z = Field()
-    b = Field()
-    a = Field()
-
-class Model3(Model1):
-    p = Field()
-    q = Field()
-
-
-print([f for f in Model1().declared_fields])
-print([f for f in Model2().declared_fields])
-print([f for f in Model3().declared_fields])
+class Field(BaseField):
+    __metaclass__ = _MetaField
---
+++ nibbles/tests/test_field_ordering.py
@@ -0,0 +1,58 @@
+from __future__ import absolute_import
+
+from unittest import TestCase
+
+from ..fields import Field
+
+# A bunch of classes for testing.
+class Ordered(Field):
+    a = Field()
+    b = Field()
+    c = Field()
+    d = Field()
+
+
+class Reversed(Field):
+    d = Field()
+    c = Field()
+    b = Field()
+    a = Field()
+
+
+class Random(Field):
+    c = Field()
+    a = Field()
+    b = Field()
+    d = Field()
+
+
+class SubClass(Ordered):
+    x = Field()
+    y = Field()
+
+
+class SubField(Field):
+    q = Field()
+    r = Reversed()
+
+
+class Test1(TestCase):
+    def test_ordered_fields(self):
+        f = Ordered()
+        self.assertEqual(['a', 'b', 'c', 'd'], f.declared_fields.keys())
+
+    def test_reversed_fields(self):
+        f = Reversed()
+        self.assertEqual(['d', 'c', 'b', 'a'], f.declared_fields.keys())
+
+    def test_random_fields(self):
+        f = Random()
+        self.assertEqual(['c', 'a', 'b', 'd'], f.declared_fields.keys())
+
+    def test_subclass_fields(self):
+        f = SubClass()
+        self.assertEqual(['a', 'b', 'c', 'd', 'x', 'y'], f.declared_fields.keys())
+
+    def test_subfield_fields(self):
+        f = SubField()
+        self.assertEqual(['q', 'r'], f.declared_fields.keys())
